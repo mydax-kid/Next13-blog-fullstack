8427c9e68d3172b3eec10f2b19b3593799492509
samuel <samuelwachira007@gmail.com>
2023-10-03 04:25:56 +0300

    first commit

---
+++ src/utils/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected successfully.");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connect;
